6e347fc6a6a272970dfe0d498339fbbce902ac6b
mgalimb <galimbertimarcelo@gmail.com>
2022-02-18 11:37:55 -0300

    Primer commit

---
+++ js/hover.js
@@ -0,0 +1,18 @@
+$("#game1").hover(function(){
+   $(this).parent().css("background","#8d99ae");
+},
+                  function(){
+                  $(this).parent().css("background","rgb(157, 154, 42)");
+});
+$("#game2").hover(function(){
+   $(this).parent().css("background","#8d99ae");
+},
+                  function(){
+                  $(this).parent().css("background","rgb(157, 154, 42)");
+});
+$("#game3").hover(function(){
+   $(this).parent().css("background","#8d99ae");
+},
+                  function(){
+                  $(this).parent().css("background","rgb(157, 154, 42)");
+});
---
+++ js/menu.js
@@ -0,0 +1,20 @@
+var btnMenu = document.getElementById("btn-menu");
+var nav = document.getElementById("nav");
+
+btnMenu.addEventListener('click', function(){
+    nav.classList.toggle('show');
+});
+
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+    var destino = $(this.hash);
+    if (destino.length == 0) {
+      destino = $('a[name="' + this.hash.substr(1) + '"]');
+    }
+    if (destino.length == 0) {
+      destino = $('html');
+    }
+    $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+    return false;
+  })
+})
