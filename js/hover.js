3599040f0388446d4716c5c96b0e93ff88d2114b
mgalim <galimbertimarcelou@gmail.com>
2022-02-18 17:13:39 -0300

    update de hover.js

--- js/hover.js
+++ js/hover.js
@@ -1,18 +1,18 @@
-$("#game1").hover(function(){
+$("#site1").hover(function(){
    $(this).parent().css("background","#8d99ae");
 },
                   function(){
                   $(this).parent().css("background","rgb(157, 154, 42)");
 });
-$("#game2").hover(function(){
+$("#site2").hover(function(){
    $(this).parent().css("background","#8d99ae");
 },
                   function(){
                   $(this).parent().css("background","rgb(157, 154, 42)");
 });
-$("#game3").hover(function(){
+$("#site3").hover(function(){
    $(this).parent().css("background","#8d99ae");
 },
                   function(){
                   $(this).parent().css("background","rgb(157, 154, 42)");
-});
+})
